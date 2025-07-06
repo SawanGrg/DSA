fc83507fb7a73325ee95b71c66a7951f2bf16c31
SawanGurung <grgsawan123@gmail.com>
2025-07-06 14:08:18 +0545

    BST: insertion

---
+++ src/main/java/org/example/bst/TreeNode.java
@@ -0,0 +1,14 @@
+package org.example.bst;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value) {
+        this.val = value;
+        this.left = null;
+        this.right = null;
+    }
+}
